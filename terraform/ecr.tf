09f37b2f1c8212a3d82d0d448692e9b3a761d66a
vaputra <vaputra@ncsu.edu>
2023-12-08 21:01:52 -0500

    Add ecr lifecycle policy and rename repo

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,8 +1,31 @@
-resource "aws_ecr_repository" "simple_prayer_service_ecr" {
-  name                 = "simple-prayer-service-ecr"
+resource "aws_ecr_repository" "simple_prayer_service" {
+  name                 = "simple-prayer-service"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "simple_prayer_service_ecr_policy" {
+  repository = aws_ecr_repository.simple_prayer_service.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
 }
