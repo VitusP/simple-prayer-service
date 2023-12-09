f04fda011911a089cd2090297c3a475d2d24546c
vaputra <vaputra@ncsu.edu>
2023-12-08 22:06:27 -0500

    Add S3 as a tfstate storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket = "simple-prayer-service-terraform-state"
+    key    = "prod/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
