e75462b485a64a39bd706a77c744a5f3d8cce325
vaputra <vaputra@ncsu.edu>
2023-12-13 20:20:57 -0500

    Add dynamodb table

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,35 @@
+########################################################################################################################
+## Create log group for our service
+########################################################################################################################
+
+resource "aws_dynamodb_table" "simple-prayer-service-prayers" {
+  name         = "prayers"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PrayerTitle"
+  range_key    = "PrayerCategory"
+
+  attribute {
+    name = "PrayerTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "PrayerCategory"
+    type = "S"
+  }
+
+  attribute {
+    name = "PrayerText"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  tags = {
+    Name        = "simple-prayer-service-prayers-dynamodb"
+    Environment = "production"
+  }
+}
