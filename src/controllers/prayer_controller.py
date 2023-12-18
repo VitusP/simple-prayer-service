ed96c799d28b0c088803f5a0c04d2584103e6377
vaputra <vaputra@ncsu.edu>
2023-12-17 21:11:09 -0500

    Add random prayer api

--- src/controllers/prayer_controller.py
+++ src/controllers/prayer_controller.py
@@ -1,5 +1,6 @@
 # /app/src/controllers/prayers_controller.py
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from ..service.prayer_service import get_random_prayer
 import boto3
 
 prayer_bp = Blueprint("prayer", __name__)
@@ -7,19 +8,11 @@ prayer_bp = Blueprint("prayer", __name__)
 
 @prayer_bp.route("/prayer")
 def get_prayer():
-    dynamodb = boto3.resource("dynamodb")
-
-    # Define the primary key value for the item you want to retrieve
-    table_name = "prayers"
-    table = dynamodb.Table(table_name)
-
-    # Perform a get_item on the DynamoDB table
-    response = table.get_item(Key={"PrayerTitle": "Saint Michael Prayer"})
-
-    # Process the get_item response
-    item = response.get("Item")
-    if item:
-        # Process the retrieved item as needed
-        return f"Item: {item}"
+    prayer = get_random_prayer()
+    if prayer is not None:
+        return (
+            jsonify({"PrayerTitle": prayer.get_title, "PrayerText": prayer.get_text}),
+            200,
+        )
     else:
-        return "Item not found"
+        return jsonify({"error": "Unable to return a prayer"}), 500
