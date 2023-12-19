842d0764188f4cbbfa601839b3cf66fbcd26d7fb
vaputra <vaputra@ncsu.edu>
2023-12-19 14:14:47 -0500

    fix dynamodb config

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -5,8 +5,8 @@
 resource "aws_dynamodb_table" "simple_prayer_service_prayers" {
   name         = "prayers"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "PrayerTitle"
-  range_key    = "PrayerCategory"
+  hash_key     = "PrayerCategory"
+  range_key    = "PrayerId"
 
   attribute {
     name = "PrayerCategory"
