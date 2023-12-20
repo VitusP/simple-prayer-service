38fe592c93a4d017d06af1a758699879e0429949
vaputra <vaputra@ncsu.edu>
2023-12-20 12:54:23 -0500

    Update endpoint

--- src/service/prayer_service.py
+++ src/service/prayer_service.py
@@ -3,25 +3,27 @@ from botocore.exceptions import ClientError
 from ..model.prayer_model import Prayer
 import logging
 import random
+from decimal import Decimal
 
 # logging
 logger = logging.getLogger("gunicorn.error")
 
 PRAYER_TABLE = "prayers"
-PRAYER_TABLE_KEY = "PrayerTitle"
+PRAYER_TABLE_KEY = "PrayerCategory"
+PRAYER_TABLE_SORT_KEY = "PrayerId"
 REGION = "us-west-2"
 
 dynamodb = boto3.resource("dynamodb", region_name="us-west-2")
 
 
-def get_prayer(prayerTitle: str) -> Prayer:
+def get_prayer(prayerCategory: str) -> Prayer:
     try:
 
         # Define the primary key value for the item you want to retrieve
         table = dynamodb.Table(PRAYER_TABLE)
 
         # Perform a get_item on the DynamoDB table
-        response = table.get_item(Key={PRAYER_TABLE_KEY: prayerTitle})
+        response = table.get_item(Key={PRAYER_TABLE_KEY: prayerCategory})
 
         # Process the get_item response
         item = response.get("Item")
@@ -40,24 +42,35 @@ def get_prayer(prayerTitle: str) -> Prayer:
 
 def get_random_prayer() -> Prayer:
     try:
-
         # Define the primary key value for the item you want to retrieve
         table = dynamodb.Table(PRAYER_TABLE)
 
         # Generate a random number within the range of total items
-        response = table.scan()
-        items_list = response.get("Items", [])
-        items_length = len(items_list)
-        random_index = random.randint(0, items_length - 1)
-        logger.info(f"Scanned {items_length} prayers in prayer table: {items_list}")
+        randId = Decimal(str(random.random()))
 
-        # Process the get_item response
-        item = items_list[random_index] if items_list[random_index] else None
-        if item:
+        response = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key(PRAYER_TABLE_KEY).eq(
+                "Core"
+            )
+            & boto3.dynamodb.conditions.Key(PRAYER_TABLE_SORT_KEY).gt(randId),
+            Limit=1,
+        )
+
+        if response["Items"]:
             # Process the retrieved item as needed
-            return Prayer(item["PrayerTitle"], item["PrayerText"])
+            prayer_item = response["Items"][0]
+            return Prayer(prayer_item["PrayerTitle"], prayer_item["PrayerText"])
         else:
-            return Prayer("not_found", "not_found")
+            response = table.query(
+                KeyConditionExpression=boto3.dynamodb.conditions.Key(
+                    PRAYER_TABLE_KEY
+                ).eq("Core")
+                & boto3.dynamodb.conditions.Key(PRAYER_TABLE_SORT_KEY).lt(randId),
+                ScanIndexForward=False,
+                Limit=1,
+            )
+            prayer_item = response["Items"][0]
+            return Prayer(prayer_item["PrayerTitle"], prayer_item["PrayerText"])
     except ClientError as e:
         # Handle the error
         error_code = e.response["Error"]["Code"]
